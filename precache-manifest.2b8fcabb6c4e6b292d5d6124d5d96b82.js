069c821c8ed780f2035ae883054c7e2d09efd6c8
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-07-28 14:47:53 +0530

    Updates

---
+++ precache-manifest.2b8fcabb6c4e6b292d5d6124d5d96b82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d957e4f7d85cf20c7b70d7b0554302a",
+    "url": "/crio-crio-2010shagun/index.html"
+  },
+  {
+    "revision": "438172d66588ca8487c6",
+    "url": "/crio-crio-2010shagun/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "493a98bc95d4e75e0920",
+    "url": "/crio-crio-2010shagun/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "438172d66588ca8487c6",
+    "url": "/crio-crio-2010shagun/static/js/2.4b777daf.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/crio-crio-2010shagun/static/js/2.4b777daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493a98bc95d4e75e0920",
+    "url": "/crio-crio-2010shagun/static/js/main.67acac1a.chunk.js"
+  },
+  {
+    "revision": "faae7acbfbdfa948f2a2",
+    "url": "/crio-crio-2010shagun/static/js/runtime-main.fc99fc00.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/crio-crio-2010shagun/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/crio-crio-2010shagun/static/media/crio-logo.63be71d0.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crio-crio-2010shagun/precache-manifest.2b8fcabb6c4e6b292d5d6124d5d96b82.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crio-crio-2010shagun/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
